c872138fe9d22bf7ff8da6cd961d258d1d52180e
Gustavo Isac <gustavoisacmn@gmail.com>
2025-05-30 19:55:52 -0300

    Add files via upload

--- script.pgcad.js
+++ script.pgcad.js
@@ -265,6 +265,30 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location.href = "fichas.html";
     });
     });
+
+    const campoCPF = document.getElementById("cpf");
+
+    campoCPF.addEventListener("input", function (e) {
+        let valor = e.target.value;
+
+        // Remove tudo que não for número
+        valor = valor.replace(/\D/g, "");
+
+        // Aplica a máscara de CPF
+        if (valor.length > 3 && valor.length <= 6) {
+            valor = valor.replace(/(\d{3})(\d+)/, "$1.$2");
+        } else if (valor.length > 6 && valor.length <= 9) {
+            valor = valor.replace(/(\d{3})(\d{3})(\d+)/, "$1.$2.$3");
+        } else if (valor.length > 9) {
+            valor = valor.replace(/(\d{3})(\d{3})(\d{3})(\d+)/, "$1.$2.$3-$4");
+        }
+
+        // Limita o número máximo de caracteres
+        valor = valor.substring(0, 14);
+
+        e.target.value = valor;
+    });
+
     
 
 });
